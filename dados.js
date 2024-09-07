5fe5322613b68e403b1e92efa5de1e2ec62c6af2
marlaoforti <marlaoforti@gmail.com>
2024-09-07 18:54:25 -0300

    ok

---
+++ dados.js
@@ -0,0 +1,51 @@
+let dados= [
+    
+    {
+
+        titulo : "Frango à Passarinho com Batata Doce",
+        descricao : "Uma receita deliciosa e fácil de preparar, perfeita para um jantar em família. O frango fica crocante por fora e suculento por dentro, acompanhado de batatas doces assadas.",
+        tags: "frango assado batata doce receita fácil jantar",
+        ingredientes: " ingredientes: frango temperado, batata doce, azeite, sal, pimenta do reino",
+        modoDePreparo: " Modo de preparo: Tempere o frango com sal e pimenta. Aqueça o azeite em uma panela e doure o frango. Adicione a batata doce cortada em cubos e cozinhe até dourar. Sirva quente.",
+
+    },
+    {
+
+        
+        titulo : "Bife à Passarinho com Batata Doce",
+        descricao : "Uma receita deliciosa e fácil de preparar, perfeita para um jantar em família. O frango fica crocante por fora e suculento por dentro, acompanhado de batatas doces assadas.",
+        tags: "frango assado batata doce receita fácil jantar",
+        ingredientes: " ingredientes: frango temperado, batata doce, azeite, sal, pimenta do reino",
+        modoDePreparo: " Modo de preparo: Tempere o frango com sal e pimenta. Aqueça o azeite em uma panela e doure o frango. Adicione a batata doce cortada em cubos e cozinhe até dourar. Sirva quente.",
+
+
+    },
+    {
+
+        titulo: "Bolo de Chocolate Simples",
+        descricao: "Um bolo de chocolate úmido e saboroso, perfeito para qualquer ocasião. Fácil de fazer e agrada a todos.",
+        tags: "bolo chocolate receita fácil sobremesa",
+        ingredientes: "ovos, açúcar, chocolate em pó, farinha de trigo, óleo, fermento em pó",
+        modoDePreparo: "Bata os ovos com o açúcar até obter um creme claro. Adicione o chocolate, a farinha e o óleo, alternando com o leite. Por último, acrescente o fermento. Asse em forno preaquecido até dourar.'"
+    },
+
+    {
+        titulo: "Pizza de Calabresa",
+        descricao: "Uma pizza clássica e saborosa, perfeita para um jantar em família.",
+        tags: "pizza calabresa receita fácil jantar",
+        ingredientes: "massa de pizza, molho de tomate, queijo mussarela, calabresa",
+        modoDePreparo: "Abra a massa de pizza, espalhe o molho de tomate, coloque a mussarela e a calabresa. Leve ao forno até dourar."
+      },
+      {
+        titulo: "Suco de Laranja",
+        descricao: "Um suco natural e refrescante, ideal para começar o dia.",
+        tags: "suco laranja receita fácil bebida",
+        ingredientes: "laranjas, água",
+        modoDePreparo: "Lave bem as laranjas, corte-as ao meio e esprema o suco. Coe e adicione água a gosto."
+      },
+      
+      
+    
+   
+];
+
