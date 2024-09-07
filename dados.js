0d60e44d35f7a9b35c2a4811e608541723ca3212
marlaoforti <marlaoforti@gmail.com>
2024-09-07 19:50:30 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -9,17 +9,7 @@ let dados= [
         modoDePreparo: " Modo de preparo: Tempere o frango com sal e pimenta. Aqueça o azeite em uma panela e doure o frango. Adicione a batata doce cortada em cubos e cozinhe até dourar. Sirva quente.",
 
     },
-    {
-
-        
-        titulo : "Bife à Passarinho com Batata Doce",
-        descricao : "Uma receita deliciosa e fácil de preparar, perfeita para um jantar em família. O frango fica crocante por fora e suculento por dentro, acompanhado de batatas doces assadas.",
-        tags: "frango assado batata doce receita fácil jantar",
-        ingredientes: " ingredientes: frango temperado, batata doce, azeite, sal, pimenta do reino",
-        modoDePreparo: " Modo de preparo: Tempere o frango com sal e pimenta. Aqueça o azeite em uma panela e doure o frango. Adicione a batata doce cortada em cubos e cozinhe até dourar. Sirva quente.",
-
-
-    },
+  
     {
 
         titulo: "Bolo de Chocolate Simples",
@@ -43,9 +33,167 @@ let dados= [
         ingredientes: "laranjas, água",
         modoDePreparo: "Lave bem as laranjas, corte-as ao meio e esprema o suco. Coe e adicione água a gosto."
       },
-      
-      
+
+      {
+        titulo: "Bolo de Chocolate Vegano",
+        descricao: "Um bolo de chocolate úmido e saboroso, perfeito para quem busca uma opção vegana. Sem leite, ovos ou manteiga, ele agrada a todos os paladares.",
+        tags: "bolo vegano chocolate sobremesa receita fácil",
+        ingredientes: "farinha de trigo, açúcar, cacau em pó, óleo vegetal, água, fermento em pó",
+        modoDePreparo: "Misture os ingredientes secos. Em outro recipiente, misture os ingredientes úmidos. Combine as duas misturas e asse em forno pré-aquecido."
+    },
+    {
+        titulo: "Lasanha de Berinjela",
+        descricao: "Uma alternativa vegetariana à tradicional lasanha, com berinjela grelhada, ricota e molho de tomate caseiro. Leve e saborosa!",
+        tags: "lasanha vegetariana berinjela receita saudável",
+        ingredientes: "berinjela, ricota, molho de tomate, massa para lasanha, queijo vegano (opcional)",
+        modoDePreparo: "Monte a lasanha em camadas, intercalando berinjela grelhada, ricota e molho de tomate. Leve ao forno até dourar."
+    },
+    {
+        titulo: "Sopa de Lentilha",
+        descricao: "Uma sopa nutritiva e reconfortante, perfeita para os dias frios. Rica em proteínas e fibras, a lentilha é um ingrediente versátil e saboroso.",
+        tags: "sopa lentilha receita saudável inverno",
+        ingredientes: "lentilha, cenoura, cebola, alho, caldo de legumes, azeite, sal, pimenta do reino",
+        modoDePreparo: "Refogue a cebola e o alho. Adicione a lentilha, a cenoura e o caldo de legumes. Cozinhe até a lentilha ficar macia."
+    },
+
+      {
+    titulo: "Strogonoff de Frango Cremoso",
+    descricao: "Um clássico da culinária brasileira, feito com frango desfiado, cogumelos salteados e um creme de leite delicioso. Perfeito para um jantar especial.",
+    tags: "strogonoff frango receita fácil jantar cremoso",
+    ingredientes: "peito de frango, cogumelos frescos, cebola, alho, extrato de tomate, mostarda, creme de leite, batata palha",
+    modoDePreparo: "Cozinhe o frango e desfie. Refogue a cebola e o alho. Adicione os cogumelos e o extrato de tomate. Por último, acrescente o creme de leite e a mostarda. Sirva com batata palha."
+    },
+
+    {
+        titulo: "Churrasco de Fraldinha com Chimichurri",
+        descricao: "Um churrasco suculento e saboroso, com fraldinha macia e um chimichurri caseiro para acompanhar. Ideal para um churrasco em família ou com amigos.",
+        tags: "churrasco fraldinha chimichurri receita fácil fim de semana",
+        ingredientes: "fraldinha, sal grosso, pimenta do reino, alho, azeite, vinagre, orégano, coentro",
+        modoDePreparo: "Tempere a fraldinha com sal grosso e pimenta do reino. Prepare o chimichurri com os ingredientes indicados. Assar a fraldinha na churrasqueira e servir com o chimichurri."
+    },
+
+    {
+        titulo: "Ragu de Carne com Massa",
+        descricao: "Um ragu de carne tradicional italiano, feito com carne bovina, tomate, vinho tinto e especiarias. Perfeito para acompanhar massas como espaguete ou penne.",
+        tags: "ragu carne massa receita italiana jantar",
+        ingredientes: "carne bovina picada, cebola, cenoura, salsão, vinho tinto, tomate pelado, extrato de tomate, especiarias",
+        modoDePreparo: "Refogue a carne com a cebola, cenoura e salsão. Adicione o vinho tinto e deixe reduzir. Acrescente o tomate pelado, extrato de tomate e especiarias. Cozinhe em fogo baixo até a carne ficar macia."
+    },
+
+    {
+        titulo: "Salmão com Crosta de Gergelim e Molho Tartare",
+        descricao: "Um prato sofisticado e fácil de preparar. O salmão fica crocante por fora e suculento por dentro, acompanhado de um molho tartare clássico.",
+        tags: "salmão receita fácil jantar sofisticado",
+        ingredientes: "filé de salmão, gergelim branco e preto, azeite, sal, pimenta do reino, maionese, picles, cebola roxa, alcaparras",
+        modoDePreparo: "Tempere o salmão com sal e pimenta. Passe no azeite e cubra com gergelim. Asse no forno até dourar. Prepare o molho tartare misturando todos os ingredientes."
+    },
+
+    {
+        titulo: "Moqueca de Peixe Baiano",
+        descricao: "Um prato típico da Bahia, com um sabor intenso e ingredientes frescos. O peixe é cozido em um caldo de leite de coco com pimentões, cebola e coentro.",
+        tags: "moqueca peixe baiano receita tradicional",
+        ingredientes: "peixe branco em postas, leite de coco, azeite de dendê, cebola, pimentão verde, pimentão vermelho, coentro, azeite, sal",
+        modoDePreparo: "Refogue a cebola, pimentões e coentro no azeite de dendê. Adicione o peixe e o leite de coco. Cozinhe até o peixe estar cozido."
+    },
+
+    {
+        titulo: "Filé de Pescada Branca com Batatas Assadas e Brócolis",
+        descricao: "Um prato leve e nutritivo, perfeito para um almoço saudável. O peixe é assado com temperos simples e acompanhado de batatas e brócolis.",
+        tags: "pescada branca batata assada brócolis receita saudável",
+        ingredientes: "filé de pescada branca, batata, brócolis, azeite, limão, sal, pimenta do reino",
+        modoDePreparo: "Tempere o peixe com limão, sal e pimenta. Asse no forno. Enquanto isso, asse as batatas e o brócolis temperados com azeite, sal e pimenta."
+    },
+
+    {
+        titulo: "Macarrão ao Pesto com Frango Grelhado",
+        descricao: "Uma opção leve e saborosa, com macarrão integral, pesto fresco e frango grelhado. Perfeito para um almoço saudável.",
+        tags: "macarrão pesto frango grelhado receita saudável almoço",
+        ingredientes: "macarrão integral, frango, manjericão fresco, pinhões, queijo parmesão, azeite, alho",
+        modoDePreparo: "Cozinhe o macarrão. Prepare o pesto no liquidificador com todos os ingredientes. Grelhe o frango. Misture o macarrão com o pesto e sirva com o frango."
+    },
+
+
+    {
+        titulo: "Ravioli de Ricota com Salvia e Manteiga",
+        descricao: "Um prato sofisticado e elegante, com ravioli recheado de ricota e um molho de salvia e manteiga. Ideal para ocasiões especiais.",
+        tags: "ravioli ricota salvia manteiga receita sofisticada jantar",
+        ingredientes: "ravioli de ricota, manteiga, salvia fresca, queijo parmesão",
+        modoDePreparo: "Cozinhe o ravioli em água fervente. Derreta a manteiga em uma panela e adicione as folhas de sálvia. Misture o ravioli com o molho e sirva com queijo parmesão."
+    },
+
+    {
+        titulo: "Hambúrguer Clássico com Queijo e Bacon",
+        descricao: "Um hambúrguer suculento e saboroso, com queijo derretido e bacon crocante. Um clássico que agrada a todos.",
+        tags: "hamburguer clássico queijo bacon receita fácil",
+        ingredientes: "carne bovina moída, pão de hambúrguer, queijo cheddar, bacon, alface, tomate, cebola, maionese, mostarda",
+        modoDePreparo: "Modele os hambúrgueres e asse na grelha ou frigideira. Torre o pão, adicione os ingredientes e monte o sanduíche."
+    },
+
+    {
+        titulo: "Hambúrguer Veggie com Cogumelos e Queijo Vegan",
+        descricao: "Uma opção deliciosa e saudável para quem busca um hambúrguer sem carne. Com cogumelos, queijo vegano e legumes frescos.",
+        tags: "hamburguer vegetariano cogumelos queijo vegano receita saudável",
+        ingredientes: "lentilha cozida, cogumelos picados, cebola caramelizada, pão de hambúrguer, queijo vegano, alface, tomate, molho especial",
+        modoDePreparo: "Misture a lentilha cozida com os cogumelos e cebola caramelizada. Modele os hambúrgueres e asse na grelha. Monte o sanduíche com os demais ingredientes."
+    },
+
+
+    {
+        titulo: "Hambúrguer Artesanal com Molho Barbecue",
+        descricao: "Um hambúrguer gourmet com blend de carnes especiais e um molho barbecue caseiro. Perfeito para quem busca um sabor mais sofisticado.",
+        tags: "hamburguer artesanal molho barbecue receita gourmet",
+        ingredientes: "blend de carnes (picanha, fraldinha), pão brioche, queijo cheddar, cebola caramelizada, molho barbecue caseiro, alface",
+        modoDePreparo: "Prepare o blend de carnes e modele os hambúrgueres. Asse na grelha e adicione os demais ingredientes. Sirva com batatas rústicas."
+    },
+
+    {
+        titulo: "Pizza Marguerita Clássica",
+        descricao: "A pizza marguerita é um clássico italiano, com tomate, mussarela e manjericão fresco, perfeita para quem busca um sabor autêntico.",
+        tags: "pizza marguerita italiana tomate mussarela manjericão",
+        ingredientes: "massa de pizza, molho de tomate, mussarela, manjericão fresco, azeite, sal",
+        modoDePreparo: "Abra a massa de pizza, espalhe o molho de tomate, cubra com mussarela e manjericão. Leve ao forno pré-aquecido até a massa dourar e o queijo derreter. Finalize com um fio de azeite e sal."
+    },
+    {
+        titulo: "Pizza de Calabresa com Cebola Caramelizada",
+        descricao: "Uma pizza saborosa e irresistível, com calabresa em rodelas e cebola caramelizada, perfeita para os amantes de sabores intensos.",
+        tags: "pizza calabresa cebola caramelizada saborosa",
+        ingredientes: "massa de pizza, molho de tomate, calabresa em rodelas, cebola, açúcar, azeite, orégano, sal",
+        modoDePreparo: "Abra a massa de pizza, espalhe o molho de tomate. Em uma panela, refogue a calabresa e a cebola até a cebola caramelizar. Cubra a pizza com a calabresa e a cebola caramelizada. Leve ao forno pré-aquecido até dourar. Finalize com orégano e sal."
+    },
+    {
+        titulo: "Pizza Vegetariana com Cogumelos e Rúcula",
+        descricao: "Uma opção leve e saborosa para os vegetarianos, com cogumelos salteados e rúcula fresca, perfeita para um jantar leve.",
+        tags: "pizza vegetariana cogumelos rúcula leve",
+        ingredientes: "massa de pizza, molho de tomate, cogumelos frescos, rúcula, queijo vegano (opcional), azeite, alho, sal",
+        modoDePreparo: "Abra a massa de pizza, espalhe o molho de tomate. Salteie os cogumelos com alho e azeite. Cubra a pizza com os cogumelos salteados e a rúcula. Se desejar, adicione queijo vegano. Leve ao forno pré-aquecido até dourar. Finalize com sal a gosto."
+    },
+
+
+    {
+        titulo: "Bife Wellington",
+        descricao: "Um clássico da culinária francesa, o Bife Wellington é um corte de filé mignon envolto em massa folhada e cogumelos, resultando em um prato sofisticado e saboroso.",
+        tags: "bife wellington carne bovina receita sofisticada jantar especial",
+        ingredientes: "filé mignon, cogumelos paris, patê, massa folhada, ovos, sal, pimenta do reino",
+        modoDePreparo: "Modo de preparo: Tempere o filé mignon com sal e pimenta. Refogue os cogumelos. Espalhe o patê sobre o filé e cubra com os cogumelos. Enrole em massa folhada e pincele com ovo. Asse em forno preaquecido até dourar."
+    },
+    {
+        titulo: "Strogonoff de Carne",
+        descricao: "Um prato tradicional brasileiro, o strogonoff de carne é uma opção prática e saborosa para o dia a dia. A carne é cozida em um molho cremoso com champignon e acompanhada de arroz branco.",
+        tags: "strogonoff carne bovina receita fácil dia a dia",
+        ingredientes: "carne magra, cebola, alho, extrato de tomate, creme de leite, mostarda, champignon, arroz branco",
+        modoDePreparo: "Modo de preparo: Refogue a carne com cebola e alho. Adicione o extrato de tomate, creme de leite, mostarda e champignon. Cozinhe até engrossar. Sirva com arroz branco."
+    },
+    {
+        titulo: "Ragu à Bolonhesa",
+        descricao: "Um molho italiano clássico, o ragu à bolonhesa é preparado com carne bovina moída, tomate, vinho tinto e especiarias. Perfeito para acompanhar massas como spaghetti ou lasanha.",
+        tags: "ragu à bolonhesa carne bovina receita italiana massa",
+        ingredientes: "carne bovina moída, cebola, cenoura, aipo, vinho tinto, tomate pelado, extrato de tomate, especiarias",
+        modoDePreparo: "Modo de preparo: Refogue a carne moída com cebola, cenoura e aipo. Adicione o vinho tinto, tomate pelado, extrato de tomate e especiarias. Cozinhe em fogo baixo por algumas horas até o molho engrossar."
+    }
+
+
+
+
     
    
 ];
-
