a991f635fd00359380fb2dc122e56202e2ae590f
marlaoforti <marlaoforti@gmail.com>
2024-09-07 18:53:49 -0300

    ok

--- app.js
+++ app.js
@@ -1,24 +1,61 @@
-// script.js
-const receitas = {
-    "ovo leite": "Omelete", 
-    "farinha leite": "Panqueca",
-    "carne tomate": "Strogonoff",
-    "cenoura chocolate" : " Bolo de Cenoura",
-    "frango tomate" : "Strogonoff de Frango",
-    "Carne Cebola" : "Carne Acebolada",
-    // Adicione mais receitas aqui
-  };
-  
-  function buscarReceita() {
-    const ingrediente1 = document.getElementById('ingrediente1').value.toLowerCase();
-    const ingrediente2 = document.getElementById('ingrediente2').value.toLowerCase();
-    const chave = `${ingrediente1} ${ingrediente2}`;
-  
-    const resultado = document.getElementById('resultado');
-    if (receitas[chave]) {
-      resultado.textContent = `Sugestão: ${receitas[chave]}
-        `;
-    } else {
-      resultado.textContent = "Não encontramos uma receita para esses ingredientes. Tente novamente.";
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo pesquisa").value
+
+    // se campoPesquisa for uma string sem nada
+    if (!campoPesquisa){
+
+        section.innerHTML = "<p> Nada foi encotrado. Você precisa digitar algo</p>"
+        return
+
     }
-  }
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados HTML
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    // Itera sobre cada dado da lista de dados
+    for (let dado of dados) {
+
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        ingredientes = dado.ingredientes.toLowerCase()
+
+        if (titulo.includes(campoPesquisa) 
+        || descricao.includes(campoPesquisa) 
+        || tags.includes(campoPesquisa)
+        || ingredientes.includes(campoPesquisa)) {
+
+            // Cria uma nova div para cada resultado, formatando o HTML com template literals
+        resultados += `
+        <div class="item-resultado">
+            <h2>
+                <a href="#" target="_blank">${dado.titulo}</a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <p class="descricao-meta">${dado.ingredientes}</p>
+            <p class="descricao-meta">${dado. modoDePreparo}</p>
+
+
+            
+        </div>
+    `;
+
+        }
+              
+    }
+    if (!resultados){
+        resultados = "<p> Nada foi encotrado</p>"
+    }
+
+
+    // Atualiza o conteúdo HTML da seção com os resultados construídos
+    section.innerHTML = resultados;
+}
+
